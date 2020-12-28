4e083e7e6f80c8b698dc903f168151944168c10b
Adame <Adame.haouari@gmail.com>
2020-12-28 16:46:37 +0100

    fin exo

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+// let nom = "Haouari"
+
+// let prenom = " Adame"
+
+// let ville = prompt("Ville")
+
+// alert(ville)
+
+
+let nom = "Haouari"
+
+let prenom = "Adame"
+
+let ville = "Tanger"
+
+let villearv = "Ikebukuro"
+
+console.log(`Le voyage de ${ville} vers ${villearv} a été enregistré au nom de ${prenom} ${nom}`)
+
+let pre = prompt ('quel est votre prenom?') 
+console.log(pre)  
+let no = prompt ('quel est votre nom?')
+console.log(no)  
+let villed = prompt ('quel est votre ville de départ?') 
+console.log(villed)  
+let dest = prompt ('quel est votre destination?') 
+console.log(dest)
+
